7934ac89c05127ccf6ba076634ff971c984d2177
George Heineman <heineman@cs.wpi.edu>
2015-11-20 22:50:36 -0500

    update numbers

--- SedgewickAlgorithms4ed/src/algs/days/day13/Dominant.java
+++ SedgewickAlgorithms4ed/src/algs/days/day13/Dominant.java
@@ -7,7 +7,7 @@ public class Dominant {
 
 	static int numComparisons = 0;
 
-	// Equation for #comparisons is:
+	// Equation for #comparisons when n is a power of 2.
 	// C(n) = 2*C(n/2) + n
 	public static int dominant(int[] collection, int lo, int hi) {
 		// one element only
@@ -40,7 +40,7 @@ public class Dominant {
 	public static void main(String[] args) {
 		
 		// Run and see how the ratio converges to 1....
-		StdOut.println("N\tDom.\tMaxCompares\tn log n\tRatio");
+		StdOut.println("N\tMaxCmp\tn log n\tRatio");
 		for (int n = 4, idx =2 ; n <= 1048576; n*= 2, idx++) {
 			int nlogn = n*idx;
 			int[] set = new int[n];
@@ -65,7 +65,35 @@ public class Dominant {
 
 			}
 			float ratio = (nlogn*1.0f)/maxCompares;
-			StdOut.println(n + "\t" + dominant(set, 0, n-1) + "\t" + maxCompares + "\t" + nlogn + "\t" + ratio);
+			StdOut.println(n + "\t" + maxCompares + "\t" + nlogn + "\t" + ratio);
+		}
+		
+		// final test. valdiate with non-power 2 numbers. Note that the n log n will 
+		// overestimate, even when using floor. Can you do better?
+		StdOut.println("N\tMaxCmp\tn Log n");
+		for (int n = 4; n <= 1048576; n*= 2) {
+			int idx;
+			int[] set = new int[n+1];
+			
+			idx = 0;
+			while (idx < n/2 - 1) {
+				set[idx] = 0;
+				idx++;
+			}
+			while (idx < n-1) {
+				set[idx] = 1;
+				idx++;
+			}
+			
+			// majority is 0
+			numComparisons = 0;
+			if (dominant(set, 0, n) != 0) {
+				StdOut.println ("ERROR");
+			}
+			
+			int nlogn = (int)((n+1)*(Math.log(n+1)/Math.log(2)));
+			float ratio = (float)(Math.floor(nlogn)/numComparisons);
+			StdOut.println((n+1) + "\t" + numComparisons + "\t" + nlogn + "\t" + (nlogn-numComparisons) + "\t" + ratio);
 		}
 	}
 }
