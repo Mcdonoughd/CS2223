e4b212328a1dba7f24f4a9b8782f2307978cd2ee
George Heineman <heineman@cs.wpi.edu>
2015-11-23 13:39:37 -0500

    lectures and handout

--- SedgewickAlgorithms4ed/src/algs/days/day16/BST.java
+++ SedgewickAlgorithms4ed/src/algs/days/day16/BST.java
@@ -1,5 +1,7 @@
 package algs.days.day16;
 
+import edu.princeton.cs.algs4.StdOut;
+
 public class BST<Key extends Comparable<Key>, Value> {
 
 	Node root;               // root of the tree
@@ -15,9 +17,13 @@ public class BST<Key extends Comparable<Key>, Value> {
 			this.val = val;
 			this.N = N;
 		}
+		
+		public String toString() { return "[" + key + "]"; }
 	}
 
 	public boolean isEmpty() { return size() == 0; }
+	
+	public String toString() { return "<bst: root=" + root +", N=" + size(root) + ">"; }
 
 	/** Return number of key-value pairs in ST. */
 	public int size()                { return size(root); }
@@ -59,6 +65,80 @@ public class BST<Key extends Comparable<Key>, Value> {
 		else              parent.val   = val;
 		
 		parent.N = 1 + size(parent.left) + size(parent.right);
+		return parent;
+	}
+	
+	// new methods for discussion
+	public Key min() { return min(root).key; }
+	
+	private Node min (Node parent) {
+		if (parent.left == null) { return parent; }
+		return min(parent.left);
+	}
+	
+	public Key nonRecursiveMin() {
+		Node n = root;
+		
+		while (n.left != null) {
+			n = n.left;
+		}
+		
+		return n.key;
+	}
+	
+	public Key floor(Key key) {
+        Node rc = floor(root, key);
+        if (rc == null) return null;
+        return rc.key;
+    } 
+
+    private Node floor(Node parent, Key key) {
+    	if (parent == null) return null;
+        
+        int cmp = key.compareTo(parent.key);
+        if (cmp == 0) return parent;                   // found? Then this is floor
+        if (cmp <  0) return floor(parent.left, key);  // smaller? must be in left subtree
+        
+        Node t = floor(parent.right, key);             // greater? we might be floor, but
+        if (t != null) return t;                       // only if 
+        else return parent; 
+    } 
+ 
+    // traversal ideas
+    // invoke an inorder traversal of the tree
+    public void inorder() { inorder(root); }
+    private void inorder(Node n) {
+    	if (n != null) {
+    		inorder (n.left);
+    		StdOut.println (n.key);
+    		inorder (n.right);
+    	}
+    }
+    
+    // traversal ideas
+    // invoke a pre-order traversal of the tree
+    public void preorder() { preorder(root); }
+    private void preorder(Node n) {
+    	if (n != null) {
+    		StdOut.println (n.key);
+    		
+    		preorder (n.left);
+    		preorder (n.right);
+    	}
+    }
+    
+    /** Implement method to return Value when removing largest element. */
+	public void deleteMin() {
+		if (root != null) { root = deleteMin(root);	}
+	}
+	
+	Node deleteMin(Node parent) {
+		if (parent.left == null) {
+			return parent.right;
+		}
+		
+		parent.left = deleteMin(parent.left);
+		parent.N = size(parent.left) + size(parent.right) + 1;
 		return parent;
 	}
 }
