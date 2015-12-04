ab7791647c32e609210a27d5ac19be2be308511f
George Heineman <heineman@cs.wpi.edu>
2015-12-04 12:45:10 -0500

    lecture 21 ready to go

---
+++ SedgewickAlgorithms4ed/src/algs/days/day21/DFSSearchAnimation.java
@@ -0,0 +1,50 @@
+package algs.days.day21;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class DFSSearchAnimation extends AnimationEngine {
+
+	boolean explore (int s, int target) {
+		Stack<Integer> stack = new Stack<Integer>();
+		color = new int[N];
+
+		// positions in the Queue are Gray and under investigation.
+		stack.push(s);
+		while (!stack.isEmpty()) {
+			Integer u = stack.pop();
+
+			for (int v : g.adj(u)) {
+				if (color[v] == White) {
+					color[v] = Gray;
+					StdDraw.setPenColor(colors[color[v]]);
+					StdDraw.filledCircle(points[v].x, points[v].y, Radius);
+					StdDraw.show(delay);
+					
+					if (v == target) {
+						color[v] = Black;
+						StdDraw.setPenColor(colors[color[v]]);
+						StdDraw.filledCircle(points[v].x, points[v].y, Radius);
+						return true;
+					}
+
+					stack.push(v);
+				}
+			}
+			
+			color[u] = Black;
+			StdDraw.setPenColor(colors[color[u]]);
+			StdDraw.filledCircle(points[u].x, points[u].y, Radius);
+			StdDraw.show(delay);
+		}
+
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			StdRandom.setSeed(Long.parseLong(args[0]));
+		}
+		new DFSSearchAnimation().launch();
+	}
+} 
---
+++ SedgewickAlgorithms4ed/src/algs/days/day21/GuidedSearchAnimation.java
@@ -0,0 +1,55 @@
+package algs.days.day21;
+
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class GuidedSearchAnimation extends AnimationEngine {
+
+	boolean explore (int s, int target) {
+		MinPQ<Point> pq = new MinPQ<Point>(N);
+		color = new int[N];
+		
+		// set known target in Point for comparisons.
+		Point.target = points[target];
+		for (int i = 0; i < N; i++) {
+			points[i].id = i;
+		}
+
+		// positions in the Queue are Gray and under investigation.
+		pq.insert(points[s]);
+		while (!pq.isEmpty()) {
+			Point p = pq.delMin();
+			for (int v : g.adj(p.id)) {
+				if (color[v] == White) {
+					color[v] = Gray;
+					StdDraw.setPenColor(colors[color[v]]);
+					StdDraw.filledCircle(points[v].x, points[v].y, Radius);
+					StdDraw.show(delay);
+					
+					if (v == target) {
+						color[v] = Black;
+						StdDraw.setPenColor(colors[color[v]]);
+						StdDraw.filledCircle(points[v].x, points[v].y, Radius);
+						return true;
+					}
+
+					pq.insert(points[v]);
+				}
+			}
+			
+			color[p.id] = Black;
+			StdDraw.setPenColor(colors[color[p.id]]);
+			StdDraw.filledCircle(p.x, p.y, Radius);
+			StdDraw.show(delay);
+		}
+
+		return false;
+	}
+	
+	public static void main(String[] args) {
+		if (args.length > 0) {
+			StdRandom.setSeed(Long.parseLong(args[0]));
+		}
+		new GuidedSearchAnimation().launch();
+	}
+} 
