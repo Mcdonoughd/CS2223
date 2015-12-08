4465122227710d5fef95013a86d1a6dff886d1c8
George Heineman <heineman@cs.wpi.edu>
2015-12-07 19:45:49 -0500

    done with day22 topological lecture

--- SedgewickAlgorithms4ed/src/algs/days/day22/DirectedDFS.java
+++ SedgewickAlgorithms4ed/src/algs/days/day22/DirectedDFS.java
@@ -1,38 +1,50 @@
 package algs.days.day22;
 
+// modified to store reverse of post order visit
+
 public class DirectedDFS {
-    boolean[] marked;  // marked[v] = true if v is reachable
-    int[]     edgeTo;
-    
-    /** Conduct DFS over digraph from specific vertex s. */
-    public DirectedDFS(Digraph G, int s) {
-        marked = new boolean[G.V()];
-        edgeTo = new int[G.V()];
-        dfs(G, s);
-    }
+	boolean[]        marked;  // marked[v] = true if v is reachable
+	int[]            edgeTo;
+	Stack<Integer>   reversePost;
 
-    /** Computes the vertices in digraph connected from multiple sources. */
-    public DirectedDFS(Digraph G, Iterable<Integer> sources) {
-        marked = new boolean[G.V()];
-        for (int v : sources) {
-            if (!marked[v]) {
-            	dfs(G, v);
-            }
-        }
-    }
+	/** Conduct DFS over digraph from specific vertex s. */
+	public DirectedDFS(Digraph G, int s) {
+		marked = new boolean[G.V()];
+		edgeTo = new int[G.V()];
+		reversePost = new Stack<Integer>();
+		dfs(G, s);
+	}
 
-    void dfs(Digraph G, int v) { 
-        marked[v] = true;
-        for (int w : G.adj(v)) {
-            if (!marked[w]) {
-            	edgeTo[w] = v;
-            	dfs(G, w);
-            }
-        }
-    }
+	/** Computes the vertices in digraph connected from all unmarked vertices. */
+	public DirectedDFS(Digraph G) {
+		marked = new boolean[G.V()];
+		edgeTo = new int[G.V()];
+		reversePost = new Stack<Integer>();
+		for (int v = 0; v < G.V(); v++) {
+			if (!marked[v]) {
+				dfs(G, v);
+			}
+		}
+	}
 
-    /** Check whether specific vertex is reachable. */
-    public boolean marked(int v) {
-        return marked[v];
-    }
+	void dfs(Digraph G, int v) { 
+		marked[v] = true;
+		for (int w : G.adj(v)) {
+			if (!marked[w]) {
+				edgeTo[w] = v;
+				dfs(G, w);
+			}
+		}
+
+		// amazingly, this can be used to recover/restore the topological ordering
+		reversePost.push(v);
+	}
+
+	/** Check whether specific vertex is reachable. */
+	public boolean marked(int v) {
+		return marked[v];
+	}
+
+	/** Retrieve reverse of postorder traversal. */
+	public Iterable<Integer> reversePostorder() { return reversePost; }
 }
---
+++ SedgewickAlgorithms4ed/src/algs/days/day22/Topological.java
@@ -0,0 +1,45 @@
+package algs.days.day22;
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Topological {
+	Iterable<Integer> order;  // topological order
+	int[] rank;               // rank[v] = position of vertex v in topological order
+
+	/** Determines whether the digraph <tt>G</tt> has a topological order and, if so,
+	 * finds such a topological order. */
+	public Topological(Digraph G) {
+		DirectedCycle finder = new DirectedCycle(G);
+		if (!finder.hasCycle()) {
+			DirectedDFS dfs = new DirectedDFS(G);
+			order = dfs.reversePostorder();
+			rank = new int[G.V()];
+			int i = 0;
+			for (int v : order)
+				rank[v] = i++;
+		}
+	}
+
+	/** Returns a topological order if the digraph has a topologial order. */
+	public boolean hasOrder() { return order != null; }
+	public Iterable<Integer> order() { return order; }
+
+	/** The the rank of vertex v in the topological order. */
+	public int rank(int v) {
+		if (hasOrder()) return rank[v];
+		else            return -1;
+	}
+
+	public static void main(String[] args) {
+		In in = new In(args[0]);
+		Digraph dg = new Digraph(in);
+		Topological top = new Topological(dg);
+		if (top.hasOrder()) {
+			for (int i : top.order()) {
+				StdOut.print(i + " ");
+			}
+			StdOut.println();
+		}
+	}
+}
