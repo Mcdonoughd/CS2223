36b37b242b84164b95173601d5d0b05d2e746d31
George Heineman <heineman@cs.wpi.edu>
2018-03-08 21:07:44 -0500

    preparing for first week of lectures

---
+++ SedgewickAlgorithms4ed/src/algs/days/day04/DoublingTest.java
@@ -0,0 +1,41 @@
+package algs.days.day04;
+
+import algs.days.day02.ThreeSumModified;
+import edu.princeton.cs.algs4.*;
+
+// page 177 of Sedgewick, 4ed
+
+public class DoublingTest {
+    private static final int MAXIMUM_INTEGER = 1000000;
+
+    // This class should not be instantiated.
+    private DoublingTest() { }
+
+    /**
+     * Returns the amount of time to call <tt>ThreeSum.count()</tt> with <em>N</em>
+     * random 6-digit integers.
+     * @param N the number of integers
+     * @return amount of time (in seconds) to call <tt>ThreeSum.count()</tt>
+     *   with <em>N</em> random 6-digit integers
+     */
+    public static double timeTrial(int N) {
+        int[] a = new int[N];
+        for (int i = 0; i < N; i++) {
+            a[i] = StdRandom.uniform(-MAXIMUM_INTEGER, MAXIMUM_INTEGER);
+        }
+        Stopwatch timer = new Stopwatch();
+        ThreeSumModified.count(a);
+        return timer.elapsedTime();
+    }
+
+    /**
+     * Prints table of running times to call <tt>ThreeSum.count()</tt>
+     * for arrays of size 250, 500, 1000, 2000, and so forth.
+     */
+    public static void main(String[] args) { 
+        for (int N = 250; true; N += N) {
+            double time = timeTrial(N);
+            StdOut.printf("%7d %5.1f\n", N, time);
+        } 
+    } 
+} 
---
+++ SedgewickAlgorithms4ed/src/algs/days/day05/FixedCapacityQueueOfStrings.java
@@ -0,0 +1,48 @@
+package algs.days.day05;
+
+import edu.princeton.cs.algs4.*;
+
+public class FixedCapacityQueueOfStrings  {
+	private String[] a;  // holds the items
+	private int N;       // number of items in queue
+
+	// create an empty queue with given capacity
+	public FixedCapacityQueueOfStrings(int capacity) {
+		a = new String[capacity];
+		N = 0;
+	}
+
+	public boolean isEmpty()            {  return N == 0;                    }
+	public boolean isFull()             {  return N == a.length;             }
+	public void enqueue(String item)    {  a[N++] = item;                    }
+	
+	
+	public String dequeue()             { 
+		String val = a[0];
+		for (int i = 0; i < N; i++) {
+			a[i] = a[i+1];
+		}
+		a[--N] = null;
+		return val;
+	}
+
+	public static void main(String[] args) {
+		FixedCapacityQueueOfStrings queue = new FixedCapacityQueueOfStrings(10);
+		while (!StdIn.isEmpty()) {
+			String item = StdIn.readString();
+			if (item.equals("-")) {
+				if (queue.isEmpty()) {
+					StdOut.println ("BAD INPUT");
+				} else {
+					StdOut.println ("Dequeued " + queue.dequeue());
+				}
+			} else {
+				queue.enqueue(item);
+			}
+		}
+		StdOut.println();
+
+		// print what's left on the queue
+		StdOut.println("(" + queue.N + " left on the queue)");
+	} 
+} 
