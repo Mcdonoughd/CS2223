feb7fc37083e2f27eaf404a261a881573418c20b
George Heineman <heineman@cs.wpi.edu>
2018-04-10 09:33:56 -0400

    avl demo stuff

---
+++ SedgewickAlgorithms4ed/src/algs/days/day18/AVLDemo.java
@@ -0,0 +1,15 @@
+package algs.days.day18;
+
+public class AVLDemo {
+	public static void main(String[] args) {
+		AVL<Integer> avl = new AVL<Integer>();
+		avl.insert(50);
+		avl.insert(30);
+		avl.insert(10);   // what happens here. Rotate Right
+		
+		avl = new AVL<Integer>();
+		avl.insert(10);
+		avl.insert(50);
+		avl.insert(30);   // what happens here.
+	}
+}
