da776fd9e5510490d82b5e3cc6bbc3089d49076d
George Heineman <heineman@cs.wpi.edu>
2018-04-10 09:54:36 -0400

    finish AVL demo

--- SedgewickAlgorithms4ed/src/algs/days/day18/AVLDemo.java
+++ SedgewickAlgorithms4ed/src/algs/days/day18/AVLDemo.java
@@ -5,11 +5,21 @@ public class AVLDemo {
 		AVL<Integer> avl = new AVL<Integer>();
 		avl.insert(50);
 		avl.insert(30);
-		avl.insert(10);   // what happens here. Rotate Right
+		avl.insert(10);   // what happens here? Rotate Right
+		
+		avl = new AVL<Integer>();
+		avl.insert(50);
+		avl.insert(10);
+		avl.insert(30);   // what happens here? Rotate Left-Right
 		
 		avl = new AVL<Integer>();
 		avl.insert(10);
 		avl.insert(50);
-		avl.insert(30);   // what happens here.
+		avl.insert(30);   // what happens here? Rotate Right-Left
+		
+		avl = new AVL<Integer>();
+		avl.insert(10);
+		avl.insert(30);
+		avl.insert(50);   // what happens here? Rotate Left
 	}
 }
