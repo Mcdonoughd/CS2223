8d653be38cf9e07f6a668de6d8c3e8a29aa9bc8e
George Heineman <heineman@cs.wpi.edu>
2018-04-26 09:51:00 -0400

    updates

--- SedgewickAlgorithms4ed/src/algs/days/day26/Recurrence.java
+++ SedgewickAlgorithms4ed/src/algs/days/day26/Recurrence.java
@@ -2,7 +2,7 @@ package algs.days.day26;
 
 public class Recurrence {
 	public static int compute (int N, int k) {
-		if (k < 1) { return 0; }
+		if (k <= 1) { return 0; }
 
 		int count = 0;
 		for (int j = 0; j < Math.sqrt(N); j++) {
@@ -14,7 +14,9 @@ public class Recurrence {
 
 	public static void main(String[] args) {
 		for (int N = 2; N < 65536*16; N*= 2) {
-			System.out.printf("%d\t%d\n", N, compute(N, N));
+			// Model is technically Ceiling(Sqrt(n)) * Ceiling(log_2(N))
+			double model = Math.ceil(Math.sqrt(N))* Math.ceil(Math.log(N)/Math.log(2));
+			System.out.printf("%d\t%d\t%.2f\n", N, compute(N, N), model);
 		}
 	}
 	
